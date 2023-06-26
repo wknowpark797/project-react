9fa0b32b13a297b2969d66c61ac7a3ca439657e3
wknowpark797 <wknowpark797@gmail.com>
2023-06-26 13:14:52 +0900

    router 이동 메뉴 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,34 @@
+import { Link, NavLink } from 'react-router-dom';
+
 function Header() {
-	return <header>Header</header>;
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<ul id='gnb'>
+				<li>
+					<NavLink to='/department'>department</NavLink>
+				</li>
+				<li>
+					<NavLink to='/community'>community</NavLink>
+				</li>
+				<li>
+					<NavLink to='/gallery'>gallery</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube'>youtube</NavLink>
+				</li>
+				<li>
+					<NavLink to='/contact'>contact</NavLink>
+				</li>
+				<li>
+					<NavLink to='/member'>member</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
