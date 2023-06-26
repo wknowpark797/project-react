bf6169a447caa9a12819fc88c45f6991976d3341
wknowpark797 <wknowpark797@gmail.com>
2023-06-26 12:48:32 +0900

    모든 페이지 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+import Visual from './components/main/Visual';
+import Community from './components/sub/Community';
+import Contact from './components/sub/Contact';
+import Department from './components/sub/Department';
+import Gallery from './components/sub/Gallery';
+import Member from './components/sub/Member';
+import Youtube from './components/sub/Youtube';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+
+			<Visual />
+			<Department />
+			<Community />
+			<Gallery />
+			<Youtube />
+			<Contact />
+			<Member />
+
+			<Footer />
+		</>
 	);
 }
 
