45617f03db1f5912f292c77d9b7f41e9dab7f53d
wknowpark797 <wknowpark797@gmail.com>
2023-06-27 13:08:31 +0900

    youtube 서브 페이지 axios로 데이터 가져오기

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,7 +1,36 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function Youtube() {
-	return <Layout name={'Youtube'}></Layout>;
+	const [VideoList, setVideoList] = useState([]);
+	console.log(VideoList);
+
+	useEffect(() => {
+		const key = 'AIzaSyDwb_57BfoNHLxlZ-Mwn2O3VNVt2tFNNMw';
+		const list = 'PLEJLcTMBRARd4AKwM7CM_0gf2mKviNR3J';
+		const num = 10;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
+
+		axios.get(url).then((data) => {
+			setVideoList(data.data.items);
+		});
+	}, []);
+
+	return (
+		<Layout name={'Youtube'}>
+			{VideoList.map((video, idx) => {
+				return (
+					<article key={idx}>
+						<img src={video.snippet.thumbnails.standard.url} alt={video.snippet.resourceId.videoId} />
+						<h2>{video.snippet.title}</h2>
+						<p>{video.snippet.description}</p>
+						<span>{video.snippet.publishedAt}</span>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default Youtube;
