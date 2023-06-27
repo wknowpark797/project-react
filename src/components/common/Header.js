5009210ffb5ee3b049bd7f39a38d4e00182ee62b
wknowpark797 <wknowpark797@gmail.com>
2023-06-27 11:53:16 +0900

    header, footer 컴포넌트 반응형 적용

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,6 @@
 import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Header({ type }) {
 	const activeClass = 'on';
@@ -44,6 +46,8 @@ function Header({ type }) {
 					</NavLink>
 				</li>
 			</ul>
+
+			<FontAwesomeIcon icon={faBars} />
 		</header>
 	);
 }
