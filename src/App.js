7a4ade9052ad6887c572f04666d7ed8c4f45b7e5
wknowpark797 <wknowpark797@gmail.com>
2023-06-27 10:35:05 +0900

    footer 영역 스타일 적용

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import './scss/style.scss';
 function App() {
 	return (
 		<>
-			{/* Switch는 내부에 중복되는 라우트 경로가 있을 때 더 구체적인 라우터를 채택하고 나머지는 무시 */}
+			{/* Switch는 내부에 중복되는 라우트 경로가 있을 때 먼저 작성된 라우터를 채택하고 나머지는 무시 */}
 			<Switch>
 				<Route exact path='/'>
 					{/* 메인 전용 라우터 Header */}
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,10 @@
 function Footer() {
-	return <footer>Footer</footer>;
+	return (
+		<footer>
+			<h1>LOGO</h1>
+			<p>2023 DCODELAB &copy; ALL RIGHTS RESERVED.</p>
+		</footer>
+	);
 }
 
 export default Footer;
