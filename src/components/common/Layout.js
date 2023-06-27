36d4b39098096be3bfcaba2d0903e08d637a4dd2
wknowpark797 <wknowpark797@gmail.com>
2023-06-27 11:07:01 +0900

    공통 서브 페이지 children 추가

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,12 +1,15 @@
 // 공통 서브 페이지
 
-function Layout({ name }) {
+function Layout({ name, children }) {
 	return (
 		<section className={`content ${name}`}>
 			<figure></figure>
 
 			<div className='inner'>
 				<h1>{name}</h1>
+
+				{/* children - 호출하는 요소에 자식요소 전달 */}
+				{children}
 			</div>
 		</section>
 	);
--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,7 +1,11 @@
 import Layout from '../common/Layout';
 
 function Department() {
-	return <Layout name={'Department'}></Layout>;
+	return (
+		<Layout name={'Department'}>
+			<p>Department</p>
+		</Layout>
+	);
 }
 
 export default Department;
