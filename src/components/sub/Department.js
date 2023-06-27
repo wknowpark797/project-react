ff452a997f367e34782bd30913c41bbdd0413a8c
wknowpark797 <wknowpark797@gmail.com>
2023-06-27 12:19:50 +0900

    컴포넌트 마운트시 외부 데이터 axios로 가져오기

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,9 +1,30 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function Department() {
+	const [Members, setMembers] = useState([]);
+	console.log(Members);
+
+	useEffect(() => {
+		axios.get(`${process.env.PUBLIC_URL}/DB/members.json`).then((data) => {
+			setMembers(data.data.members);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Department'}>
-			<p>Department</p>
+			{Members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+						</div>
+						<h1>{member.name}</h1>
+						<p>{member.position}</p>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
