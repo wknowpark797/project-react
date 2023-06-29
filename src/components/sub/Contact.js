a51fc475d95bd8a077f8f066db05896639a8025e
wknowpark797 <wknowpark797@gmail.com>
2023-06-29 13:06:33 +0900

    Contact.js 마커 이미지 등록

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -9,20 +9,25 @@ function Contact() {
 	// 지도가 들어갈 프레임은 가상요소 참조를 위해 useRef로 참조 객체를 생성
 	const mapContainer = useRef(null);
 
-	// 기본지도 생성하기
-	const mapOption = {
-		center: new kakao.maps.LatLng(33.450701, 126.570667),
-		level: 3,
-	};
-
 	// DOM이 완성된 후 Map 연결
 	// 인스턴스 호출 구문은 컴포넌트 처음 마운트시 호출
 	useEffect(() => {
+		// 기본지도 생성하기
+		const mapOption = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3,
+		};
 		const mapInstance = new kakao.maps.Map(mapContainer.current, mapOption);
 
 		// Marker 생성하기
+		const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+		const imageSize = new kakao.maps.Size(232, 99);
+		const imageOption = { offset: new kakao.maps.Point(116, 99) };
+		const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
 		const marker = new kakao.maps.Marker({
 			position: mapOption.center,
+			image: markerImage,
 		});
 		marker.setMap(mapInstance);
 	}, []);
