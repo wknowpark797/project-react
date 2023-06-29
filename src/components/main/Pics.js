cb06cac9f6f2ef1ecc0cda0c3422eed3dc19794a
wknowpark797 <wknowpark797@gmail.com>
2023-06-29 12:04:03 +0900

    main pics 영역 모션 추가

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,15 +1,27 @@
 function Pics({ Scrolled, Pos }) {
-	console.log(Scrolled);
+	// 쓰로틀링 없이 매번 호출되어야 한다.
+	// console.log(Scrolled);
+
+	const currentPos = Scrolled - Pos;
+	const base = window.innerHeight / 2;
+	const modifiedPos = currentPos + base;
 
 	return (
 		<section id='pics' className='myScroll'>
 			<h1
 				style={{
-					transform: `translateX(${(Scrolled - Pos) * 2}px)`,
+					transform: `translateX(${currentPos}px)`,
 				}}
 			>
 				FLICKR
 			</h1>
+
+			<article
+				style={{
+					transform: `translate(-50%, -50%) rotate(${Scrolled >= Pos - base ? modifiedPos : 0}deg) scale(${Scrolled >= Pos - base ? 1 + modifiedPos / 500 : '1'})`,
+					opacity: `${Scrolled >= Pos - base ? 1 - modifiedPos / 1000 : 1}`,
+				}}
+			></article>
 		</section>
 	);
 }
