460fbf7d9cd5a77ce23513d4b640fafcfd19a79d
wknowpark797 <wknowpark797@gmail.com>
2023-07-04 12:46:42 +0900

    Community.js Update 기능 구현

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -4,7 +4,11 @@ import { useState, useEffect, useRef } from 'react';
 function Community() {
 	const input = useRef(null);
 	const textarea = useRef(null);
+	const editInput = useRef(null);
+	const editTextarea = useRef(null);
+
 	const [Posts, setPosts] = useState([]);
+	const [Allowed, setAllowed] = useState(true);
 
 	const resetForm = () => {
 		input.current.value = '';
@@ -25,6 +29,49 @@ function Community() {
 		if (!window.confirm('해당 게시물을 삭제하시겠습니까?')) return;
 		// filter 자체가 불변성을 유지하기 때문에 복사과정이 필요없다.
 		setPosts(Posts.filter((_, idx) => idx !== index));
+	};
+
+	const enableUpdate = (index) => {
+		// 수정모드 진입함수 호출 시 Allowed가 true일 때에만 로직이 실행되도록 처리
+		if (!Allowed) return;
+		// 로직이 실행되면 allowed값을 false로 바꿔서 이후부터는 다시 수정모드로 진입되는 것을 방지
+		setAllowed(false);
+		setPosts(
+			Posts.map((post, idx) => {
+				if (index === idx) post.enableUpdate = true;
+				return post;
+			})
+		);
+	};
+
+	const disableUpdate = (index) => {
+		setPosts(
+			Posts.map((post, idx) => {
+				if (index === idx) post.enableUpdate = false;
+				return post;
+			})
+		);
+		// 글 수정 취소버튼을 눌러서 disableUpdate 함수가 호출이 되어야 Allowed값을 다시 true로 바꿔 글 수정이 가능하도록 처리
+		setAllowed(true);
+	};
+
+	const updatePost = (index) => {
+		// 유효성 검사
+		if (!editInput.current.value.trim() || !editTextarea.current.value.trim()) {
+			return alert('수정할 제목과 본문을 모두 입력하세요.');
+		}
+
+		setPosts(
+			Posts.map((post, idx) => {
+				if (index === idx) {
+					post.title = editInput.current.value;
+					post.content = editTextarea.current.value;
+					post.enableUpdate = false;
+				}
+
+				return post;
+			})
+		);
 	};
 
 	useEffect(() => {
@@ -50,15 +97,36 @@ function Community() {
 				{Posts.map((post, idx) => {
 					return (
 						<article key={idx}>
-							<div className='txt'>
-								<h2>{post.title}</h2>
-								<p>{post.content}</p>
-							</div>
+							{post.enableUpdate ? (
+								// 수정 모드
+								<>
+									<div className='txt'>
+										{/* onChange 이벤트로 제어하지 않는 input 요소의 value 값은 defaultValue 속성으로 지정 */}
+										{/* value: 리액트의 상태값에 관리되는 Form 요소, defaultValue: 일반 DOM에 의해 관리되는 Form 요소 */}
+										<input type='text' defaultValue={post.title} ref={editInput} />
+										<br />
+										<textarea cols='30' rows='3' defaultValue={post.content} ref={editTextarea}></textarea>
+									</div>
 
-							<nav className='btnSet'>
-								<button>EDIT</button>
-								<button onClick={() => deletePost(idx)}>DELETE</button>
-							</nav>
+									<nav className='btnSet'>
+										<button onClick={() => disableUpdate(idx)}>CANCEL</button>
+										<button onClick={() => updatePost(idx)}>UPDATE</button>
+									</nav>
+								</>
+							) : (
+								// 출력 모드
+								<>
+									<div className='txt'>
+										<h2>{post.title}</h2>
+										<p>{post.content}</p>
+									</div>
+
+									<nav className='btnSet'>
+										<button onClick={() => enableUpdate(idx)}>EDIT</button>
+										<button onClick={() => deletePost(idx)}>DELETE</button>
+									</nav>
+								</>
+							)}
 						</article>
 					);
 				})}
