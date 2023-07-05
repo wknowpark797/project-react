a05ef2dfd7c96dfa1c8f8cbe69241fd82469b40a
wknowpark797 <wknowpark797@gmail.com>
2023-07-05 12:40:46 +0900

    Gallery.js 타입별 데이터 가져오기 및 Masonry 설치, 적용

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,38 +1,53 @@
 import Layout from '../common/Layout';
+import Masonry from 'react-masonry-component';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function Gallery() {
-	const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-	const key = '7f259a4112d06fbef0736c84af20f014';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 20;
-	const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
-
 	const [Items, setItems] = useState([]);
 
+	const getFlickr = async (opt) => {
+		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+		const key = '7f259a4112d06fbef0736c84af20f014';
+		const method_interest = 'flickr.interestingness.getList';
+		const method_search = 'flickr.photos.search';
+		const method_user = 'flickr.people.getPhotos';
+		const num = 20;
+		// const myId = '198471371@N05';
+		let url = '';
+
+		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+		if (opt.type === 'search') url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
+		if (opt.type === 'user') url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
+
+		const result = await axios.get(url);
+		console.log(result.data.photos.photo);
+		setItems(result.data.photos.photo);
+	};
+
 	useEffect(() => {
-		axios.get(url).then((json) => {
-			console.log(json.data.photos.photo);
-			setItems(json.data.photos.photo);
-		});
+		getFlickr({ type: 'interest' });
+		// getFlickr({ type: 'search', tags: 'landscape' });
+		// getFlickr({ type: 'user', user: '198471371@N05' });
 	}, []);
 
 	return (
 		<Layout name={'Gallery'}>
 			<div className='frame'>
-				{Items.map((item, idx) => {
-					return (
-						<article key={idx}>
-							<div className='inner'>
-								<div className='pic'>
-									<img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} alt='{item.title}' />
+				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
+					{Items.map((item, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<div className='pic'>
+										<img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} alt='{item.title}' />
+									</div>
+									<h2>{item.title}</h2>
 								</div>
-								<h2>{item.title}</h2>
-							</div>
-						</article>
-					);
-				})}
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
