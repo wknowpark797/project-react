768472cf74f94dadc8aa37446caef317849d754d
wknowpark797 <wknowpark797@gmail.com>
2023-07-11 13:17:18 +0900

    Footer.js useSelector를 통한 정보 출력

--- src/redux/action.js
+++ src/redux/action.js
@@ -8,3 +8,11 @@ export const setMembers = (data) => {
 		payload: data,
 	};
 };
+
+/*
+  1. action.js -> action 생성 후 리턴
+  2. reducer.js -> action 객체를 받아서 전역 데이터를 변형한 뒤 리턴
+  3. store.js -> reducer가 반환한 객체를 전역 store 공간에 저장 후 export
+  4. index.js -> store 전역 데이터 객체를 App 컴포넌트에 Provider로 전달
+  5. 원하는 컴포넌트 어디에서든 useSelector로 store 데이터 호출 가능
+*/
