e56fb7809d72279ba8154345feb90b3eeb2d25fb
wknowpark797 <wknowpark797@gmail.com>
2023-07-17 10:05:38 +0900

    main Vids 영역 전역 유튜브 데이터 호출

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -2,11 +2,22 @@ import { memo } from 'react';
 import { useSelector } from 'react-redux';
 
 function Vids() {
-	useSelector((store) => console.log(store));
+	const Vids = useSelector((store) => store.youtube.data);
+	console.log(Vids);
 
 	return (
 		<section id='vids' className='myScroll'>
-			Vids
+			{Vids.map((vid, idx) => {
+				if (idx >= 5) return null;
+
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+						</div>
+					</article>
+				);
+			})}
 		</section>
 	);
 }
