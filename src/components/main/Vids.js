0157009ebe71153fa7426091a0ecd7e7e764a73c
wknowpark797 <wknowpark797@gmail.com>
2023-07-17 11:38:06 +0900

    main Vids 영역 Swiper 적용

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,5 +1,10 @@
 import { memo } from 'react';
 import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 function Vids() {
 	const Vids = useSelector((store) => store.youtube.data);
@@ -7,17 +12,31 @@ function Vids() {
 
 	return (
 		<section id='vids' className='myScroll'>
-			{Vids.map((vid, idx) => {
-				if (idx >= 5) return null;
+			<Swiper
+				modules={[Autoplay, Pagination, Navigation]}
+				loop={true}
+				spaceBetween={50}
+				slidesPerView={3}
+				centeredSlides={true}
+				autoplay={{ delay: 2000, disableOnInteraction: true }}
+				pagination={{ clickable: true }}
+				navigation={true}
+			>
+				{Vids.map((vid, idx) => {
+					if (idx >= 5) return null;
 
-				return (
-					<article key={idx}>
-						<div className='pic'>
-							<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
-						</div>
-					</article>
-				);
-			})}
+					return (
+						<SwiperSlide key={idx}>
+							<div className='inner'>
+								<div className='pic'>
+									<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+								</div>
+								<h2>{vid.snippet.title}</h2>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
 		</section>
 	);
 }
